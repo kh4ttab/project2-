762ec399b3adc92c38baac2e31830699b95f7dcd
kh4ttab <K9KI361@GMAIL.COM>
2024-12-07 17:28:35 +0200

    Update config.php

--- register_app/config.php
+++ register_app/config.php
@@ -2,5 +2,5 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
-    $conn= mysqli_connect('localhost','root','','login&register') or die('connection failed');
+    $conn= mysqli_connect('localhost','root','root','user_db') or die('connection failed');
 ?>
